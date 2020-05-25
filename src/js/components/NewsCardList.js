40cbc8822aa883262643c94534ab7018897dc24d
Ruslan <texs14@yandex.ru>
2020-05-25 08:51:40 +0300

    исправлено отображение кнопки "показать ещё"

--- src/js/components/NewsCardList.js
+++ src/js/components/NewsCardList.js
@@ -1,4 +1,5 @@
-import NewsCard from "./NewsCard";
+import NewsCard from './NewsCard';
+import {NEW_BUTTON} from '../constants/constants';
 
 export default class NewsCardList extends NewsCard {
     constructor(container) {
@@ -12,17 +13,19 @@ export default class NewsCardList extends NewsCard {
         cardList.classList.add('news__list');
         console.log(news);
         let arr = news.slice(this._i, this._i + 3);
-
+        console.log(arr);
         arr.forEach((elem) => {
             cardList.insertAdjacentHTML("afterbegin", this.create(elem));
             this._container.append(cardList);
         });
         this._i += 3;
+        this._i >= news.length ?  NEW_BUTTON.style.display = 'none' : ``;
     }
 
 
 
     clearList() {
+        NEW_BUTTON.style.display = 'block';
         document.querySelector('.news__list-container').innerHTML = '';
         this._i = 0;
     }
